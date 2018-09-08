8f88da2ffc2333c189743346cebadd577f36fc34
Andrew <andrew.samir@revival.sa>
2018-09-08 18:27:24 +0200

    Initial commit

---
+++ app/src/main/java/facilities/samir/andrew/facilities/activities/LoginActivity.java
@@ -0,0 +1,18 @@
+package facilities.samir.andrew.facilities.activities;
+
+import android.os.Bundle;
+import android.app.Activity;
+
+import facilities.samir.andrew.facilities.R;
+
+public class LoginActivity extends Activity
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+
+}
---
+++ app/src/main/java/facilities/samir/andrew/facilities/activities/SplashScreenActivity.java
@@ -0,0 +1,14 @@
+package facilities.samir.andrew.facilities.activities;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class SplashScreenActivity extends Activity
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+    }
+}
